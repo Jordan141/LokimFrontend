f7b2611df349c66166953e8c949048defd1f9f3c
Jordan Moss <jmoss636@gmail.com>
2018-03-03 03:18:15 +0000

    Login field refactor

--- src/components/home.js
+++ src/components/home.js
@@ -6,14 +6,29 @@ class Home extends Component {
     constructor(props){
         super(props)
         this.state = {
+            username: '',
+            password: '',
             successfulLogin: false
         }
         this.login = this.login.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
     }
-    login(){
+    login(user, pass){
         alert('Login button clicked!')
         //Login to server, if status returns 200, enter chat component
         this.setState({successfulLogin: true})
+    }
+    handleChange(event){
+        const settings = ['username', 'password']
+        //Find key for object using placeholders
+        const key = settings[event.target.placeholder]
+        //Only change state of specific value
+        this.setState({ [key] : event.target.value})
+    }
+    handleSubmit(event){
+        this.login(this.state.username, this.state.password)
+        event.preventDefault()
     }
     render(){
         if(this.state.successfulLogin){
@@ -23,18 +38,16 @@ class Home extends Component {
             <div className="App">
             <div className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <h2>Welcome to React/Electron</h2>
-                <p className="App-intro">
-                Hello LokIM User!
-                </p>
+                <h2>Welcome to Loki Instant Messenger</h2>
+                <p className="App-intro">Hello LokIM User!</p>
             </div>
-            <input className='userInput' type="text" placeholder="Username"/><br/>
-            <input className='userInput' type="password" placeholder="Password"/><br/>
-            <ul>
-                <li className="homeButton btn btn-primary" onClick={this.login}>Login</li>
-                <li className="homeButton btn btn-secondary"><Link to='/register'>Create Account</Link></li>
-            </ul>
-            <p className='info-paragraph'>LokIM connects to other users via websockets and no information is stored on the server post-emission. All data regarding a user is stored on said user's device.</p>
+            <form onSubmit={this.login}>
+            <input className='userInput' type="text" value={this.state.username} placeholder="Username" required/><br/>
+            <input className='userInput' type="password" value={this.state.password} placeholder="Password" required/><br/>
+            <input type='submit' className='homeButton btn btn-primary' value='Login'/>
+            </form>
+            <li className="homeButton btn btn-secondary"><Link to='/register'>Create Account</Link></li>
+            <p className='info-paragraph'>LokIM connects to other users via websockets through a server and very little information is stored on the server post-emission. All data regarding a user is stored on said user's device. The only information we store on the server is that which is required for essential functionality.</p>
             </div>
         )
     }
