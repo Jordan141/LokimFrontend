c7143668f9ba6e7275680284130fdcf8dcbb8d1f
Jordan Moss <jmoss636@gmail.com>
2018-03-05 20:45:55 +0000

    Trial phase, sidebar component

--- src/components/chat.js
+++ src/components/chat.js
@@ -1,19 +1,26 @@
 import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
+import {Sidebar, SidebarItem} from 'react-responsive-sidebar'
 class Chat extends Component {
     constructor(props){
         super(props)
 
         this.state = {
-            
+            items: [
+                <SidebarItem>Dashboard </SidebarItem>,
+                <SidebarItem>Profile</SidebarItem>,
+                <SidebarItem>Settings</SidebarItem>
+            ]
         }
     }
 
     render(){
         return (
-            <div className='container-fluid'>
-                <li className='btn btn-danger'><Link to='/'>Logout</Link></li>
-            </div>
+            <Sidebar content={this.state.items}>
+                <div className='container-fluid'>
+                    <li className='btn btn-danger'><Link to='/'>Logout</Link></li>
+                </div>
+            </Sidebar>
         )
     }
 }
