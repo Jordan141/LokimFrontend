0d3404ed6e6509073005896a17efa1e8fdca5fd6
Jordan Moss <jmoss636@gmail.com>
2018-03-05 20:37:57 +0000

    Removed material/ui

--- src/components/chat.js
+++ src/components/chat.js
@@ -1,8 +1,5 @@
 import React, {Component} from 'react'
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import {Link} from 'react-router-dom'
-import { RaisedButton } from 'material-ui';
-
 class Chat extends Component {
     constructor(props){
         super(props)
@@ -16,9 +13,6 @@ class Chat extends Component {
         return (
             <div className='container-fluid'>
                 <li className='btn btn-danger'><Link to='/'>Logout</Link></li>
-                <MuiThemeProvider>
-                    <RaisedButton label='Default' />
-                </MuiThemeProvider>
             </div>
         )
     }
